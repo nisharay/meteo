b4078416d7413af71f77f72ecfe56b722bb33fa6
nisharay <75917958+nisharay@users.noreply.github.com>
2025-09-24 12:44:49 -0500

    Search functionality using API

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  apiKey = "edotc63895cd1225b0fc30a76437dc70";
+  apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFromElement = document.querySelector("#search-form");
 searchFromElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
